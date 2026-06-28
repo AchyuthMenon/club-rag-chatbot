a8b884878ede28c64f3468f375c8b7c5868b6d35
AchyuthMenon <achyuthmenon06@gmail.com>
2026-06-28 21:22:21 +0530

    implemented chunking

--- backend/readPdf.js
+++ backend/readPdf.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const pdf = require("pdf-parse");
-
+const chunkText = require("./utils/chunkText");
 async function readPDF() {
   const dataBuffer = fs.readFileSync(
     "./docs/ieeePolicies.pdf"
@@ -8,7 +8,23 @@ async function readPDF() {
 
   const data = await pdf(dataBuffer);
 
-  console.log(data.text);
+  const chunks = chunkText(data.text);
+
+console.log("Number of chunks:", chunks.length);
+
+console.log("Chunks:", chunks.length);
+
+console.log("----------------");
+console.log("Chunk 1");
+console.log(chunks[0]);
+
+console.log("----------------");
+console.log("Chunk 2");
+console.log(chunks[1]);
+
+console.log("----------------");
+console.log("Chunk 3");
+console.log(chunks[2]);
 }
 
 readPDF();
